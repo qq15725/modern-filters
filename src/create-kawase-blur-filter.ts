96973971ccbf075ab798096f600ba8da425746c8
你手捧鲜花 <157215725@qq.com>
2023-04-03 15:40:57 +0800

    feat: kawaseBlurFilter support animation

--- src/create-kawase-blur-filter.ts
+++ src/create-kawase-blur-filter.ts
@@ -1,17 +1,26 @@
 import { defineFilter } from './define-filter'
+import animation from './shaders/animation.frag?raw'
 import type { AnimationFilterOptions } from './types'
 
 const fragmentShader = `
 varying vec2 vTextureCoord;
 uniform sampler2D uSampler;
 uniform vec2 uOffset;
+uniform float uTime;
+uniform float uDuration;
+uniform bool uIsOut;
+uniform bool uLoop;
+
+${ animation }
 
 void main(void) {
+  float time = normalizeTime(uTime, uDuration, uLoop, uIsOut);
+  vec2 offset = vec2(uOffset.x - linear(time, 0.0, uOffset.x, uDuration), uOffset.y - linear(time, 0.0, uOffset.y, uDuration));
   vec4 color = vec4(0.0);
-  color += texture2D(uSampler, vec2(vTextureCoord.x - uOffset.x, vTextureCoord.y + uOffset.y));
-  color += texture2D(uSampler, vec2(vTextureCoord.x + uOffset.x, vTextureCoord.y + uOffset.y));
-  color += texture2D(uSampler, vec2(vTextureCoord.x + uOffset.x, vTextureCoord.y - uOffset.y));
-  color += texture2D(uSampler, vec2(vTextureCoord.x - uOffset.x, vTextureCoord.y - uOffset.y));
+  color += texture2D(uSampler, vec2(vTextureCoord.x - offset.x, vTextureCoord.y + offset.y));
+  color += texture2D(uSampler, vec2(vTextureCoord.x + offset.x, vTextureCoord.y + offset.y));
+  color += texture2D(uSampler, vec2(vTextureCoord.x + offset.x, vTextureCoord.y - offset.y));
+  color += texture2D(uSampler, vec2(vTextureCoord.x - offset.x, vTextureCoord.y - offset.y));
   color *= 0.25;
   gl_FragColor = color;
 }
@@ -24,8 +33,11 @@ export interface KawaseBlurFilterOptions extends AnimationFilterOptions {
 
 export function createKawaseBlurFilter(options: KawaseBlurFilterOptions = {}) {
   const {
-    blur = 4,
+    blur = 10,
     quality = 3,
+    mode = 'in',
+    duration = 1.8,
+    loop = false,
   } = options
 
   const kernels = [blur]
@@ -36,6 +48,12 @@ export function createKawaseBlurFilter(options: KawaseBlurFilterOptions = {}) {
       k -= step
       kernels.push(k)
     }
+  }
+
+  const sharedUniforms = {
+    uIsOut: mode !== 'in',
+    uDuration: duration,
+    uLoop: loop,
   }
 
   return defineFilter((texture) => {
@@ -54,12 +72,12 @@ export function createKawaseBlurFilter(options: KawaseBlurFilterOptions = {}) {
         offset = kernels[i] + 0.5
         uOffset[0] = offset * uvX
         uOffset[1] = offset * uvY
-        texture.registerProgram({ fragmentShader, uniforms: { uOffset } })
+        texture.registerProgram({ fragmentShader, uniforms: { ...sharedUniforms, uOffset } })
       }
       offset = kernels[last] + 0.5
       uOffset[0] = offset * uvX
       uOffset[1] = offset * uvY
     }
-    texture.registerProgram({ fragmentShader, uniforms: { uOffset } })
+    texture.registerProgram({ fragmentShader, uniforms: { ...sharedUniforms, uOffset } })
   })
 }
